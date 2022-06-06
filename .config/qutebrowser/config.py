c011e984d6b637cf0147ffc1cc7fdd0acb47ddf0
SamDc73 <halshehadat@protonmail.ch>
2022-06-06 22:49:02 +0300

    qutebrowser config and theme

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,180 @@
+# Load automatic configuration
+# config.load_autoconfig()c.aliases = {"q": "quit", "w": "session-save", "wq": "quit --save"}
+
+# Theme
+config.source("themes/monokai.py")
+
+# Always restore open sites when qutebrowser is reopened.
+c.auto_save.session = True
+
+# Prefer dark color schemes
+c.colors.webpage.preferred_color_scheme = "dark"
+# c.colors.webpage.darkmode.enabled = True
+# config.set("colors.webpage.darkmode.enabled", True)
+
+# Require a confirmation before quitting the application if downloads are active.
+c.confirm_quit = ["downloads"]
+
+# Use neovim in kitty as an editor
+c.editor.command = [
+    "kitty",
+    "-t",
+    "Qutebrowser Editor",
+    "-e",
+    "nvim",
+    "{}",
+    "+call cursor({line},{column0})",
+]
+
+# Enable smooth scrolling
+c.scrolling.smooth = True
+
+# Allow websites to show notifications.
+# config.set('content.notifications.enabled', True, 'https://www.youtube.com')
+
+# Setting default page for when opening new tabs or new windows with
+# commands like :open -t and :open -w .
+c.url.default_page = "https://www.startpage.com/"
+c.url.start_pages = "https://www.startpage.com/"
+
+# Set up search engines
+c.url.searchengines = {
+    "DEFAULT": "https://www.startpage.com/?q={}",
+    "goog": "https://www.google.com/search?q={}",
+    "dd": "https://duckduckgo.com?q={}",
+    "gh": "https://github.com/search?q={}",
+    "hoo": "https://hoogle.haskell.org/?hoogle={}",
+    "re": "https://teddit.net/r/all/search?q={}",
+    "sp": "https://startpage.com/sp/search?query={}",
+    "yt": "https://www.youtube.com/results?search_query={}",
+}
+
+# Font settings
+c.fonts.default_family = "Source Code Pro"
+c.fonts.default_size = "11pt"
+
+
+## Binds ##
+
+# Bind for opening download immediately
+config.bind("<Ctrl-o>", "prompt-open-download", mode="prompt")
+
+# Bind Ctrl-e in command mode to edit the current command
+config.bind("<Ctrl+e>", "edit-command", mode="command")
+
+# Binds for moving through completion items
+config.bind("<Ctrl-j>", "completion-item-focus next", mode="command")
+config.bind("<Ctrl-k>", "completion-item-focus prev", mode="command")
+
+# Rebind close tab to 'dd'
+config.unbind("x")
+config.bind("x", "tab-close")
+
+# Unbind home-page key
+config.unbind("<Ctrl+h>")
+
+# Bindings to close all tabs to the left or right of the current tab
+# --pinned flag requires https://github.com/qutebrowser/qutebrowser/pull/6287
+# Merged in 2.2.0
+config.bind("co", "tab-only --pinned keep")  # Close other tabs
+config.bind("ch", "tab-only --next --pinned keep")  # Close tabs to the left
+config.bind("cl", "tab-only --prev --pinned keep")  # Close tabs to the right
+
+# Bindings to move in tab history
+config.bind("gh", "set-cmd-text -s :back ")
+config.bind("gl", "set-cmd-text -s :forward ")
+
+# Binds for qute-pass
+config.bind("zl", "spawn --userscript qute-pass")
+config.bind("zol", "spawn --userscript qute-pass --otp-only")
+config.bind("zpl", "spawn --userscript qute-pass --password-only")
+config.bind("zul", "spawn --userscript qute-pass --username-only")
+
+# Binds for images
+config.unbind(";i")
+config.bind(";ii", "hint images")
+config.bind(";io", "hint images run open {hint-url}")
+config.bind(";iO", "hint images run open -t {hint-url}")
+config.bind(
+    ";ig",
+    "hint images run open https://www.google.com/searchbyimage?&image_url={hint-url}",
+)
+
+# Bind for opening link in private window
+config.bind(";p", "hint all run open -p {hint-url}")
+
+# Open links in other applications
+config.unbind("m")
+config.bind("mm", "spawn -d mpv {url}")
+config.bind("mf", "spawn -d firefox {url}")
+config.bind("mc", "spawn -d chromium {url}")
+config.bind(";mm", "hint all spawn -d mpv {hint-url}")
+config.bind(";mf", "hint all spawn -d firefox {hint-url}")
+config.bind(";mc", "hint all spawn -d chromium {hint-url}")
+
+
+## Content ##
+
+# Don't view pdf files in-browser
+c.content.pdfjs = False
+
+# Disable autoplay
+c.content.autoplay = False
+
+# Anti-fingerprinting
+c.content.headers.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+c.content.headers.accept_language = "en-US,en;q=0.5"
+c.content.headers.custom = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+}
+c.content.cookies.accept = "no-3rdparty"
+
+# Adblock filter lists
+c.content.blocking.adblock.lists = [
+    "https://easylist.to/easylist/easylist.txt",
+    "https://easylist.to/easylist/easyprivacy.txt",
+    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+    "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt",
+    "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt",
+]
+
+## Downloads ##
+
+# Put the download bar at the bottom of the screen
+c.downloads.position = "bottom"
+
+# Remove finished downloads from the download bar after 3 minutes
+c.downloads.remove_finished = 200000
+
+# Downloads Directory
+c.downloads.location.directory = "~/Downloads"
+
+## Tabs ##
+
+# When to show the tab barabs.
+c.tabs.show = "always"
+c.tabs.show = "multiple"  # To Hide the bar if only one tab is open
+
+# Load the default page after closing the last tab
+c.tabs.last_close = "close"
+
+# Set a maximum tab width so small numbers of tabs aren't so huge
+c.tabs.max_width = 250
+
+
+## Cookies ##
+
+# Which cookies to accept. With QtWebEngine, this setting also controls
+# other features with tracking capabilities similar to those of cookies;
+# including IndexedDB, DOM storage, filesystem API, service workers, and
+# AppCache. Note that with QtWebKit, only `all` and `never` are
+# supported as per-domain values. Setting `no-3rdparty` or `no-
+# unknown-3rdparty` per-domain on QtWebKit will have the same effect as
+# `all`.
+# Type: String
+# Valid values:
+#   - all: Accept all cookies.
+#   - no-3rdparty: Accept cookies from the same origin only. This is known to break some sites, such as GMail.
+#   - no-unknown-3rdparty: Accept cookies from the same origin only, unless a cookie is already set for the domain. On QtWebEngine, this is the same as no-3rdparty.
+#   - never: Don't accept cookies at all.
+config.set("content.cookies.accept", "all", "https://www.reddit.com")
